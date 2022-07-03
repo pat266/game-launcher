c8a2a471525ad93427de005e30287553750dd94a
pat266 <69089199+pat266@users.noreply.github.com>
2022-07-03 00:29:19 -0400

    clean up the codes and add appropriate comments

--- Launcher_VLCM_lsaj/Forms/Game.cs
+++ Launcher_VLCM_lsaj/Forms/Game.cs
@@ -12,7 +12,8 @@ namespace Launcher_VLCM_niua_lsaj.Forms
 
         private void Game_Load(object sender, EventArgs e)
         {
-            axShockwaveFlash.Movie = string.Format("{0}?{1}", Program.flash_movie, Program.flash_vars); // đưa dữ liệu cần để load game vào flash control trên form game
+            // set the necessary information to flash control of form game
+            axShockwaveFlash.Movie = string.Format("{0}?{1}", Program.flash_movie, Program.flash_vars);
         }
     }
 }
