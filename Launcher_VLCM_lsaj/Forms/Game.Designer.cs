832c05f48d06d4a864c89b5cf89e72744363977c
pat266 <69089199+pat266@users.noreply.github.com>
2022-07-02 22:42:41 -0400

    initial commit of a working game launcher

---
+++ Launcher_VLCM_lsaj/Forms/Game.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Launcher_VLCM_niua_lsaj.Forms
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
+            this.axShockwaveFlash = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axShockwaveFlash
+            // 
+            this.axShockwaveFlash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axShockwaveFlash.Enabled = true;
+            this.axShockwaveFlash.Location = new System.Drawing.Point(0, 0);
+            this.axShockwaveFlash.Name = "axShockwaveFlash";
+            this.axShockwaveFlash.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash.OcxState")));
+            this.axShockwaveFlash.Size = new System.Drawing.Size(784, 561);
+            this.axShockwaveFlash.TabIndex = 0;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.axShockwaveFlash);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.KeyPreview = true;
+            this.Name = "Game";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Launcher VLCM - lsaj.niua.com";
+            this.Load += new System.EventHandler(this.Game_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash;
+
+    }
+}
---
+++ Launcher_VLCM_lsaj/Forms/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Launcher_VLCM_niua_lsaj.Forms
+{
+    public partial class Game : Form
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            axShockwaveFlash.Movie = string.Format("{0}?{1}", Program.flash_movie, Program.flash_vars); // đưa dữ liệu cần để load game vào flash control trên form game
+        }
+    }
+}
