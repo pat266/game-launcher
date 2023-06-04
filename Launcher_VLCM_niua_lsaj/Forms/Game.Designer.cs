93363dbb7353d23204bdb0f8f096fa518ba51e25
pat266 <69089199+pat266@users.noreply.github.com>
2023-06-04 15:42:39 -0400

    add axShockwaveFlashObjects dynamically in the code instead of in design

--- Launcher_VLCM_niua_lsaj/Forms/Game.Designer.cs
+++ Launcher_VLCM_niua_lsaj/Forms/Game.Designer.cs
@@ -28,43 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
-            this.axShockwaveFlash = new AxShockwaveFlashObjects.AxShockwaveFlash();
-            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).BeginInit();
-            this.SuspendLayout();
+            components = new System.ComponentModel.Container();
+            imageList1 = new System.Windows.Forms.ImageList(components);
+            FormBorder = new System.Windows.Forms.Panel();
+            reset = new CustomControls.CButton();
+            restart = new CustomControls.CButton();
+            translationButton = new CustomControls.CButton();
+            minimize = new CustomControls.CButton();
+            maximize = new CustomControls.CButton();
+            exit = new CustomControls.CButton();
+            nameLabel = new System.Windows.Forms.Label();
+            ApplicationSound = new CustomControls.CButton();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            FormBorder.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
             // 
-            // axShockwaveFlash
+            // imageList1
             // 
-            this.axShockwaveFlash.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.axShockwaveFlash.Enabled = true;
-            this.axShockwaveFlash.Location = new System.Drawing.Point(0, 0);
-            this.axShockwaveFlash.Name = "axShockwaveFlash";
-            this.axShockwaveFlash.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash.OcxState")));
-            this.axShockwaveFlash.Size = new System.Drawing.Size(784, 561);
-            this.axShockwaveFlash.TabIndex = 0;
+            imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // FormBorder
+            // 
+            FormBorder.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            FormBorder.Controls.Add(reset);
+            FormBorder.Controls.Add(restart);
+            FormBorder.Controls.Add(translationButton);
+            FormBorder.Controls.Add(minimize);
+            FormBorder.Controls.Add(maximize);
+            FormBorder.Controls.Add(exit);
+            FormBorder.Controls.Add(nameLabel);
+            FormBorder.Controls.Add(ApplicationSound);
+            FormBorder.Dock = System.Windows.Forms.DockStyle.Fill;
+            FormBorder.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            FormBorder.Location = new System.Drawing.Point(0, 0);
+            FormBorder.Margin = new System.Windows.Forms.Padding(0);
+            FormBorder.Name = "FormBorder";
+            FormBorder.Size = new System.Drawing.Size(784, 30);
+            FormBorder.TabIndex = 1;
+            // 
+            // reset
+            // 
+            reset.BackColor = System.Drawing.Color.Transparent;
+            reset.BackgroundColor = System.Drawing.Color.Transparent;
+            reset.BorderColor = System.Drawing.Color.Transparent;
+            reset.BorderRadius = 0;
+            reset.BorderSize = 0;
+            reset.Dock = System.Windows.Forms.DockStyle.Left;
+            reset.FlatAppearance.BorderSize = 0;
+            reset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            reset.ForeColor = System.Drawing.Color.Transparent;
+            reset.Location = new System.Drawing.Point(265, 0);
+            reset.Name = "reset";
+            reset.Size = new System.Drawing.Size(30, 30);
+            reset.TabIndex = 8;
+            reset.TextColor = System.Drawing.Color.Transparent;
+            reset.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            reset.UseVisualStyleBackColor = false;
+            // 
+            // restart
+            // 
+            restart.BackColor = System.Drawing.Color.Transparent;
+            restart.BackgroundColor = System.Drawing.Color.Transparent;
+            restart.BorderColor = System.Drawing.Color.Transparent;
+            restart.BorderRadius = 0;
+            restart.BorderSize = 0;
+            restart.Dock = System.Windows.Forms.DockStyle.Right;
+            restart.FlatAppearance.BorderSize = 0;
+            restart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            restart.ForeColor = System.Drawing.Color.Transparent;
+            restart.Location = new System.Drawing.Point(664, 0);
+            restart.Name = "restart";
+            restart.Size = new System.Drawing.Size(30, 30);
+            restart.TabIndex = 7;
+            restart.TextColor = System.Drawing.Color.Transparent;
+            restart.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            restart.UseVisualStyleBackColor = false;
+            // 
+            // translationButton
+            // 
+            translationButton.BackColor = System.Drawing.Color.Transparent;
+            translationButton.BackgroundColor = System.Drawing.Color.Transparent;
+            translationButton.BorderColor = System.Drawing.Color.Transparent;
+            translationButton.BorderRadius = 0;
+            translationButton.BorderSize = 0;
+            translationButton.Dock = System.Windows.Forms.DockStyle.Left;
+            translationButton.FlatAppearance.BorderSize = 0;
+            translationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            translationButton.ForeColor = System.Drawing.Color.Transparent;
+            translationButton.Location = new System.Drawing.Point(235, 0);
+            translationButton.Name = "translationButton";
+            translationButton.Size = new System.Drawing.Size(30, 30);
+            translationButton.TabIndex = 6;
+            translationButton.TextColor = System.Drawing.Color.Transparent;
+            translationButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            translationButton.UseVisualStyleBackColor = false;
+            // 
+            // minimize
+            // 
+            minimize.BackColor = System.Drawing.Color.Transparent;
+            minimize.BackgroundColor = System.Drawing.Color.Transparent;
+            minimize.BorderColor = System.Drawing.Color.Transparent;
+            minimize.BorderRadius = 0;
+            minimize.BorderSize = 0;
+            minimize.Dock = System.Windows.Forms.DockStyle.Right;
+            minimize.FlatAppearance.BorderSize = 0;
+            minimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            minimize.ForeColor = System.Drawing.Color.Transparent;
+            minimize.Location = new System.Drawing.Point(694, 0);
+            minimize.Name = "minimize";
+            minimize.Size = new System.Drawing.Size(30, 30);
+            minimize.TabIndex = 4;
+            minimize.TextColor = System.Drawing.Color.Transparent;
+            minimize.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            minimize.UseVisualStyleBackColor = false;
+            // 
+            // maximize
+            // 
+            maximize.BackColor = System.Drawing.Color.Transparent;
+            maximize.BackgroundColor = System.Drawing.Color.Transparent;
+            maximize.BorderColor = System.Drawing.Color.Transparent;
+            maximize.BorderRadius = 0;
+            maximize.BorderSize = 0;
+            maximize.Dock = System.Windows.Forms.DockStyle.Right;
+            maximize.FlatAppearance.BorderSize = 0;
+            maximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            maximize.ForeColor = System.Drawing.Color.Transparent;
+            maximize.Location = new System.Drawing.Point(724, 0);
+            maximize.Name = "maximize";
+            maximize.Size = new System.Drawing.Size(30, 30);
+            maximize.TabIndex = 3;
+            maximize.TextColor = System.Drawing.Color.Transparent;
+            maximize.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            maximize.UseVisualStyleBackColor = false;
+            // 
+            // exit
+            // 
+            exit.BackColor = System.Drawing.Color.Transparent;
+            exit.BackgroundColor = System.Drawing.Color.Transparent;
+            exit.BorderColor = System.Drawing.Color.Transparent;
+            exit.BorderRadius = 0;
+            exit.BorderSize = 0;
+            exit.Dock = System.Windows.Forms.DockStyle.Right;
+            exit.FlatAppearance.BorderSize = 0;
+            exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            exit.ForeColor = System.Drawing.Color.Transparent;
+            exit.Location = new System.Drawing.Point(754, 0);
+            exit.Name = "exit";
+            exit.Size = new System.Drawing.Size(30, 30);
+            exit.TabIndex = 2;
+            exit.TextColor = System.Drawing.Color.Transparent;
+            exit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            exit.UseVisualStyleBackColor = false;
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            nameLabel.Font = new System.Drawing.Font("Script MT Bold", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            nameLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            nameLabel.Location = new System.Drawing.Point(30, 0);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new System.Drawing.Size(205, 21);
+            nameLabel.TabIndex = 1;
+            nameLabel.Text = "Launcher - lsaj.niua.com";
+            nameLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // ApplicationSound
+            // 
+            ApplicationSound.BackColor = System.Drawing.Color.Transparent;
+            ApplicationSound.BackgroundColor = System.Drawing.Color.Transparent;
+            ApplicationSound.BorderColor = System.Drawing.Color.Transparent;
+            ApplicationSound.BorderRadius = 0;
+            ApplicationSound.BorderSize = 0;
+            ApplicationSound.Dock = System.Windows.Forms.DockStyle.Left;
+            ApplicationSound.FlatAppearance.BorderSize = 0;
+            ApplicationSound.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            ApplicationSound.ForeColor = System.Drawing.Color.Transparent;
+            ApplicationSound.Location = new System.Drawing.Point(0, 0);
+            ApplicationSound.Name = "ApplicationSound";
+            ApplicationSound.Size = new System.Drawing.Size(30, 30);
+            ApplicationSound.TabIndex = 0;
+            ApplicationSound.TextColor = System.Drawing.Color.Transparent;
+            ApplicationSound.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            ApplicationSound.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(FormBorder, 0, 0);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new System.Drawing.Size(784, 561);
+            tableLayoutPanel1.TabIndex = 2;
             // 
             // Game
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.axShockwaveFlash);
-            this.DoubleBuffered = true;
-            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.ForeColor = System.Drawing.Color.Black;
-            this.KeyPreview = true;
-            this.Name = "Game";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Launcher VLCM - lsaj.niua.com";
-            this.Load += new System.EventHandler(this.Game_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash)).EndInit();
-            this.ResumeLayout(false);
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.White;
+            ClientSize = new System.Drawing.Size(784, 561);
+            Controls.Add(tableLayoutPanel1);
+            DoubleBuffered = true;
+            Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            ForeColor = System.Drawing.Color.Black;
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            KeyPreview = true;
+            MinimumSize = new System.Drawing.Size(150, 150);
+            Name = "Game";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Launcher VLCM - lsaj.niua.com";
+            Load += Game_Load;
+            FormBorder.ResumeLayout(false);
+            FormBorder.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
-
-        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash;
-
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Panel FormBorder;
+        private CustomControls.CButton translationButton;
+        private CustomControls.CButton minimize;
+        private CustomControls.CButton maximize;
+        private CustomControls.CButton exit;
+        private System.Windows.Forms.Label nameLabel;
+        private CustomControls.CButton ApplicationSound;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private CustomControls.CButton restart;
+        private CustomControls.CButton reset;
     }
 }
--- Launcher_VLCM_niua_lsaj/Forms/Game.cs
+++ Launcher_VLCM_niua_lsaj/Forms/Game.cs
@@ -1,18 +1,50 @@
-﻿using System;
+﻿using AxShockwaveFlashObjects;
+using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Launcher_VLCM_niua_lsaj.Forms
 {
     public partial class Game : Form
     {
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash;
         public Game()
         {
             InitializeComponent();
+            Initialize_Flash();
+        }
+
+        private void Initialize_Flash()
+        {
+            // Create the object
+            this.axShockwaveFlash = new AxShockwaveFlashObjects.AxShockwaveFlash();
+
+            // Set properties
+            this.axShockwaveFlash.Dock = DockStyle.Fill;
+            this.axShockwaveFlash.Enabled = true;
+            this.axShockwaveFlash.Location = new Point(3, 33);
+            this.axShockwaveFlash.Name = "axShockwaveFlash";
+            this.axShockwaveFlash.Size = new Size(778, 525);
+            this.axShockwaveFlash.TabIndex = 2;
+            // Add the component to the form's control collection
+            this.tableLayoutPanel1.Controls.Add(this.axShockwaveFlash, 0, 1);
         }
 
         private void Game_Load(object sender, EventArgs e)
         {
-            axShockwaveFlash.Movie = string.Format("{0}?{1}", Program.flash_movie, Program.flash_vars); // đưa dữ liệu cần để load game vào flash control trên form game
+            // set the necessary information to flash control of form game
+            string movie = string.Format("{0}?{1}", Program.flash_movie, Program.flash_vars);
+            // axShockwaveFlash.Movie = movie;
+            // axShockwaveFlash.LoadMovie(0, movie);
+
+
+            var localSWF = Application.StartupPath + @"\AS3Game.swf";
+            // receive data from AS3
+            // axShockwaveFlash.FlashCall += new _IShockwaveFlashEvents_FlashCallEventHandler(AS3_Receive);
+            axShockwaveFlash.LoadMovie(0, localSWF);
+            // sending movie data to AS3
+            axShockwaveFlash.CallFunction("<invoke name=\"loadMovie\" returntype=\"xml\"><arguments><string>" + movie + "</string></arguments></invoke>");
         }
     }
 }
