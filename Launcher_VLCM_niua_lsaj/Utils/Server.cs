66521be9c2ed8509a28dbaa7d7de8e4f4cb32fa8
pat266 <69089199+pat266@users.noreply.github.com>
2023-06-10 16:31:10 -0400

    add code to get upcoming game2cn servers

--- Launcher_VLCM_niua_lsaj/Utils/Server.cs
+++ Launcher_VLCM_niua_lsaj/Utils/Server.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Numerics;
-using System.Net;
-using System.Text;
 using HtmlAgilityPack;
 using System.Globalization;
 
@@ -17,11 +13,8 @@ namespace Launcher_VLCM_niua_lsaj.Utils
     public class Server
     {
         static readonly HttpClient httpClient = new HttpClient();
-        /**
-         * Helper method:
-         * Get the maximum server number
-         */
-        static async Task<List<UpcomingServer>> GetServer_Niua_Async()
+        
+        public static async Task<List<UpcomingServer>> GetServer_Niua_Async()
         {
             List<UpcomingServer> upcomingServers = new List<UpcomingServer>();
             try
@@ -71,6 +64,72 @@ namespace Launcher_VLCM_niua_lsaj.Utils
                 else
                 {
                     Console.WriteLine("No unreleased servers found");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine($"Message: {e.Message}");
+            }
+            return upcomingServers;
+        }
+
+        public static async Task<List<UpcomingServer>> GetServer_Game2cn_Async()
+        {
+            List<UpcomingServer> upcomingServers = new List<UpcomingServer>();
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://www.game2.cn/script/server/gserver.js");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                string[] lines = responseBody.Split(
+                    new string[] { "\r\n", "\r", "\n" },
+                    StringSplitOptions.None
+                );
+
+                // Removing the initial 'var gserver=' from the responseBody
+                responseBody = lines[0].Replace("var gserver=", string.Empty);
+                responseBody = responseBody.Replace(";", string.Empty);
+
+                var result = JsonConvert.DeserializeObject<Dictionary<string, List<Game2cnServer>>>(responseBody);
+
+                foreach (var item in result)
+                {
+                    // the ASCII for \u5373\u5c06\u5f00\u542f means "about to be released"
+                    var filteredGames = item.Value.Where(game => game.state == "\u5373\u5c06\u5f00\u542f" && game.code.Contains("lsaj"));
+
+                    foreach (var game in filteredGames)
+                    {
+                        // Extracting number from the code
+                        var match = Regex.Match(game.code, @"\d+");
+
+                        if (match.Success)
+                        {
+                            DateTime releaseDate = DateTime.ParseExact(game.open_date, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+
+                            TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
+                            TimeZoneInfo estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+                            DateTime releaseDateUtc = TimeZoneInfo.ConvertTimeToUtc(releaseDate, cstZone);
+                            DateTime releaseDateEst = TimeZoneInfo.ConvertTimeFromUtc(releaseDateUtc, estZone);
+
+                            // Get current time in New York
+                            DateTime currentTimeEst = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, estZone);
+
+                            // Calculate the difference in hours
+                            TimeSpan timeDifference = releaseDateEst - currentTimeEst;
+                            double hoursTillRelease = timeDifference.TotalHours;
+
+                            upcomingServers.Add(new UpcomingServer
+                            {
+                                serverNumber = int.Parse(match.Value),
+                                releaseDateEst = releaseDateEst,
+                                hoursTillRelease = hoursTillRelease
+                            });
+                            Console.WriteLine(upcomingServers[upcomingServers.Count - 1]);
+                        }
+                    }
                 }
             }
             catch (HttpRequestException e)
