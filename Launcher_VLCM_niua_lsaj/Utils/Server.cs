c293c55509ee38d7b1951cf62c484f18df8372ff
pat266 <69089199+pat266@users.noreply.github.com>
2023-06-10 16:27:10 -0400

    add new packages and helper class

---
+++ Launcher_VLCM_niua_lsaj/Utils/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher_VLCM_niua_lsaj.Utils
+{
+    public class Server
+    {
+    }
+
+    public class UpcomingServer
+    {
+        public int serverNumber { get; set; }
+        public DateTime releaseDateEst { get; set; }
+        public double hoursTillRelease { get; set; }
+        public override string ToString()
+        {
+            return $"Server Number: {serverNumber}, Release Date: {releaseDateEst} EST, Hours until release: {hoursTillRelease}";
+        }
+    }
+    public class Game2cnServer
+    {
+        public string state { get; set; }
+        public string code { get; set; }
+        public string open_date { get; set; }
+    }
+}
